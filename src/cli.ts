80b6e91f879a23c0859bf0636c14832b063912eb
Eli Barzilay <eli@barzilay.org>
2021-09-20 16:02:11 -0400

    Minor: better error when no argument is given

--- src/cli.ts
+++ src/cli.ts
@@ -5,7 +5,8 @@ import { resolve } from "path";
 
 const fileName = process.argv[2];
 if (!fileName || !existsSync(fileName)) {
-    console.error(`File ${fileName} not found - provide a path to the root project tsconfig.`);
+    console.error((fileName ? `File ${fileName} not found` : `Argument expected`)
+        + ` - provide a path to the root project tsconfig.`);
     process.exit(1);
 }
 const configPath = resolve(process.cwd(), fileName);
