d89ea0defcc669924b67e561bf9f27ee7dec5b47
Eli Barzilay <eli@barzilay.org>
2021-10-28 10:19:01 -0400

    Much work on minimizing changes
    * Re-implement `unindent.ts` using `ts-morph`, which makes it easy to do
      the exact change (unindentation) that is needed.
    
    * Many improvements to the `run-transform` script.  Also a fix: the
      diagnostics generation should happen just before running the
      conversion.
    
    * Get rid of most of the redundant empty-line deletions using
      `filter-patch.ts` which is used to tweak the patch generated by the
      conversion changes.
    
      It's possible to do more work here, for example, identify an edited
      line that just drops an end-of-line comment, or end-of-args comma.
      The extreme end here is to have a cheap "parser" that would "tokenize"
      the input where these tokens are things that the conversion adds, like
      `xxx.` (including the dot) and `/* @internal */`, then check if these
      things are only added (ignoring stuff between tokens: whitespaces,
      commas, eol-comments), and if so, plant them into the original text.
    
      But if that level is wanted, then probably just put the time into
      fixing the conversion or possibly re-implement it via `ts-morph`
      too (which requires updating it to work with the head version).

--- src/unindent.ts
+++ src/unindent.ts
@@ -1,67 +1,20 @@
-import {
-    createPrinter,
-    NewLineKind,
-    createSourceFile,
-    ScriptTarget,
-    SyntaxKind,
-    Node,
-    EmitHint,
-    ModuleDeclaration,
-    ModuleBlock,
-    SyntaxList,
-    isModuleDeclaration,
-    isModuleBlock,
-    isIdentifier,
-} from "typescript";
-import { existsSync, readFileSync } from "fs";
+import { Project } from "ts-morph";
 
-function unindent(fileName: string) {
-    const bad = (msg: string) => new Error(`${fileName}: ${msg}`);
-    if (!existsSync(fileName)) throw bad(`file not found`);
-    const text = readFileSync(fileName).toString();
-    //
-    // const onEmitNode = (hint: EmitHint, node: Node, emitCallback: (hint: EmitHint, node: Node) => void) => {
-    //     console.error(">>>");
-    //     emitCallback(hint, node);
-    //     console.error("<<<");
-    // };
-    const printer = createPrinter({
-        removeComments: false, newLine: NewLineKind.CarriageReturnLineFeed //, preserveSourceNewlines: true
-    } as any, {
-        // onEmitNode
+const project = new Project({
+    tsConfigFilePath: "src/tsconfig.json", // won't follow references
+});
+
+// get all files, which is what following the tsconfig references gets anyway,
+// except for three one-file directories with non-namespace/empty files.
+project.addSourceFilesAtPaths(["src/**/*.ts", "!**/*.d.ts"]);
+
+project.getSourceFiles().forEach(f => {
+    console.log(f.getFilePath());
+    f.getModules().forEach(m => {
+        if (m.getDeclarationKind() !== "namespace") return;
+        const b = m.getBodyOrThrow();
+        f.unindent([b.getStart(), b.getEnd()]);
     });
-    //
-    const sourceFile = createSourceFile(fileName, text, ScriptTarget.ESNext);
-    //
-    if (sourceFile.getChildCount() !== 2)
-        throw bad(`expected 2 toplevel children`);
-    if (sourceFile.getChildAt(1).kind !== SyntaxKind.EndOfFileToken)
-        throw bad(`expected second toplevel child to be EOF`);
-    if (sourceFile.getChildAt(0).kind !== SyntaxKind.SyntaxList)
-        throw bad(`expected first toplevel child to be SyntaxList`);
-    if (sourceFile.getChildAt(0).getChildCount() !== 1)
-        throw bad(`expected first toplevel child to be SyntaxList with one child`);
-    const modDecl = sourceFile.getChildAt(0).getChildAt(0);
-    if (!isModuleDeclaration(modDecl))
-        throw bad(`expected a single toplevel ModuleDeclaration`);
-    const modName = modDecl.name;
-    if (!isIdentifier(modName))
-        throw bad(`expected ModuleDeclaration to have an Identifer name`);
-    const modBlock = modDecl.body;
-    if (!modBlock)
-        throw bad(`expected ModuleDeclaration to have a body`);
-    if (!isModuleBlock(modBlock))
-        throw bad(`expected ModuleDeclaration to have a ModuleBlock`);
-    //
-    const texts = modBlock.statements.map(s =>
-        printer.printNode(EmitHint.Unspecified, s, sourceFile));
-    [`namespace ${modName.escapedText} {`, ``, ...texts, ``, `}`]
-    .forEach(line => process.stdout.write(line + "\r\n"));
-    // console.log(printer.printFile(sourceFile));
-}
+});
 
-if (!process.argv[2]) {
-    console.error(`Argument expected`);
-    process.exit(1);
-}
-unindent(process.argv[2]);
+project.saveSync();
