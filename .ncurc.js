8ce878ba75c1b321f8184484b85fedf2c5841ad3
Jake Bailey <5341706+jakebailey@users.noreply.github.com>
2022-05-25 15:36:35 -0700

    Update deps

---
+++ .ncurc.js
@@ -0,0 +1,8 @@
+/** @type {import('npm-check-updates').RunOptions}*/
+module.exports = {
+    target: (dependencyName, [{ semver, version, operator, major, minor, patch, release, build }]) => {
+        if (dependencyName === "@types/node") return "minor";
+        if (major === "0") return "minor";
+        return "latest";
+    },
+};
