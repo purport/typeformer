9b0b6f78d0c8dda74d6f11f8c8d59b05695e353c
Jake Bailey <5341706+jakebailey@users.noreply.github.com>
2022-06-03 21:04:18 -0700

    update deps

--- .ncurc.js
+++ .ncurc.js
@@ -1,5 +1,6 @@
 /** @type {import('npm-check-updates').RunOptions}*/
 module.exports = {
+    reject: "zx", // TODO: quiet hack doesn't work in newer versions.
     target: (dependencyName, [{ semver, version, operator, major, minor, patch, release, build }]) => {
         if (dependencyName === "@types/node") return "minor";
         if (major === "0") return "minor";
